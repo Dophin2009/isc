bef254c155fba5bd8fc69688e70a4e334c033b7f
Eric Zhao <21zhaoe@protonmail.com>
2020-07-29 17:32:38 -0400

    lexgen: Finish implementing basic functionality

--- lexgen/src/codegen.rs
+++ lexgen/src/codegen.rs
@@ -5,7 +5,7 @@ use std::collections::HashMap;
 use automata::dfa::{DFAFromNFA, Transition};
 use automata::{DFA, NFA};
 use proc_macro::TokenStream;
-use proc_macro2::{Span, TokenStream as TokenStream2};
+use proc_macro2::TokenStream as TokenStream2;
 use quote::quote;
 use regexp2::class::{CharClass, CharRange};
 use regexp2::parser::{NFAParser, Parser};
@@ -23,16 +23,46 @@ pub fn lexer(tok: TokenStream) -> TokenStream {
     let DFAFromNFA { dfa, nfa_mapping }: DFAFromNFA<_> = nfa.into();
 
     let dfa_static = lazy_static_dfa(&dfa);
+
+    let action_match: Vec<_> = nfa_mapping
+        .iter()
+        .filter_map(|(dfa_state, nfa_states)| {
+            action_mapping
+                .iter()
+                .filter(|(nfa_state, _)| nfa_states.contains(nfa_state))
+                .max_by_key(|(_, (_, precedence))| precedence)
+                .and_then(|(_, (action, _))| Some(quote!(#dfa_state => #action)))
+        })
+        .collect();
+
     (quote! {
         #dfa_static
 
-        #vis fn #name(input: &mut str) -> Option<#return_type> {
+        #vis fn #name(input: &str) -> std::option::Option<(#return_type, std::string::String)> {
             let (m, final_state) = match LEXER_DFA.find(&input.chars()) {
-                Some(m) => m,
-                None => return None,
+                std::option::Option::Some(m) => m,
+                std::option::Option::None => return std::option::Option::None,
             };
 
-            None
+            // If match does not start at beginning of input, should initiate error handling
+            if m.start() != 0 {
+                return std::option::Option::None;
+            }
+
+            if m.end() == m.start() {
+                return std::option::Option::None;
+            }
+
+            let #str_ident: std::string::String = input.chars().take(m.end()).collect();
+            let token = match final_state {
+                #( #action_match ),*,
+                _ => std::option::Option::None,
+            };
+
+            token.and_then(|t| {
+                let remaining = input.chars().skip(m.end()).collect();
+                std::option::Option::Some((t, remaining))
+            })
         }
 
     })
@@ -42,7 +72,7 @@ pub fn lexer(tok: TokenStream) -> TokenStream {
 const INVALID_REGEXP_ERROR: &str = "invalid regular expression";
 
 // Parse the rules into a single NFA and a map of final states to action expressions.
-fn parse_combined_nfa(rules: &Vec<Rule>) -> (NFA<CharClass>, HashMap<u32, &Expr>) {
+fn parse_combined_nfa(rules: &Vec<Rule>) -> (NFA<CharClass>, HashMap<u32, (&Expr, usize)>) {
     let nfa_parser = NFAParser::new();
     // Parse regular expression strings into NFAs.
     let nfa_sub: Vec<_> = rules
@@ -73,13 +103,15 @@ fn parse_combined_nfa(rules: &Vec<Rule>) -> (NFA<CharClass>, HashMap<u32, &Expr>
     // Combine NFAs into a single NFA.
     let mut action_mapping = HashMap::new();
     let mut nfa = NFA::new();
-    for (sub, action) in nfa_sub.iter() {
+    nfa.total_states = 0;
+    for (precedence, (sub, action)) in nfa_sub.iter().enumerate() {
         let offset = nfa.total_states;
 
         NFA::copy_into(&mut nfa, sub);
         // Map new, offsetted final states to their original action.
         for sub_final in sub.final_states.iter() {
-            action_mapping.insert(*sub_final + offset, *action);
+            nfa.final_states.insert(*sub_final + offset);
+            action_mapping.insert(*sub_final + offset, (*action, precedence));
         }
     }
 
@@ -90,7 +122,7 @@ fn lazy_static_dfa(dfa: &DFA<CharClass>) -> TokenStream2 {
     let initial_state = dfa.initial_state;
     let total_states = dfa.total_states;
     let final_states: Vec<_> = dfa.final_states.iter().collect();
-    let transitions_args: Vec<_> = dfa
+    let transition_sets: Vec<_> = dfa
         .transition
         .clone()
         .into_iter()
@@ -98,29 +130,23 @@ fn lazy_static_dfa(dfa: &DFA<CharClass>) -> TokenStream2 {
             let ranges: Vec<_> = tr
                 .ranges
                 .iter()
-                .map(|CharRange { start, end }| quote! { CharRange::new(#start, #end) })
+                .map(|CharRange { start, end }| quote!(regexp2::class::CharRange::new(#start, #end)))
                 .collect();
-            quote! { #src, [ #( CharClass::new(#ranges) ),* ], #dest }
+            quote! { dfa.transition.set(#src, automata::dfa::Transition(vec![ #( #ranges ),* ].into()), #dest); }
         })
         .collect();
 
     quote! {
-        use std::collections::HashSet;
-
-        use automata::{DFA, table::Table};
-        use regexp2::class::{CharClass, CharRange};
-        use lazy_static::lazy_static;
-
-        lazy_static! {
-            static ref LEXER_DFA: DFA<CharClass> = {
-                let mut dfa = DFA::new();
+        lazy_static::lazy_static! {
+            static ref LEXER_DFA: automata::DFA<regexp2::class::CharClass> = {
+                let mut dfa = automata::DFA::new();
                 dfa.initial_state = #initial_state;
                 dfa.total_states = #total_states;
-                dfa.final_states = HashSet::new();
+                dfa.final_states = std::collections::HashSet::new();
                 dfa.final_states.extend(&[ #( #final_states ),* ]);
 
-                dfa.transition = Table::new();
-                #( dfa.transition.set(#transitions_args) )*
+                dfa.transition = automata::table::Table::new();
+                #( #transition_sets )*
 
                 dfa
             };
