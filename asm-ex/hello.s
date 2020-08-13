fe6254e619e038931c1236ff60c0560ee19d7f10
Eric Zhao <21zhaoe@protonmail.com>
2020-08-13 17:10:09 -0400

    asm-ex: Hello world in nasm

---
+++ asm-ex/hello.s
@@ -0,0 +1,25 @@
+; use 64-bit mode
+bits 64
+
+; export entry point
+global _start
+
+; code section
+section .text
+
+_start:
+	mov rax, 1				; system call 4: write
+	mov rdi, 1				; file descriptor 1: stdout
+	mov rsi, msg			; string to write
+	mov rdx, msglen		; length of string
+	syscall						; execute system call
+
+	mov rax, 60				; system call 60: exit
+	mov rdi, 0				;
+	syscall
+
+; data section
+section .rodata
+
+	msg: db "Hello, world!", 10
+	msglen: equ $ - msg
