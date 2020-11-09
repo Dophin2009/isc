27940a451854d972275047815235ffdac4464f35
Eric Zhao <21zhaoe@protonmail.com>
2020-11-09 14:40:25 -0500

    lalr: Begin creating LL(1) parsing

--- lalr/src/grammar.rs
+++ lalr/src/grammar.rs
@@ -1,56 +1,31 @@
 use std::collections::HashMap;
 use std::hash::Hash;
-use std::iter::FromIterator;
-use std::vec;
+
+use crate::error::ConstructionError;
+use crate::ll::Parser as LLParser;
 
 pub trait SymbolType = Clone + PartialEq + Eq;
 pub trait Nonterminal = Hash + SymbolType;
 
+pub type GrammarNoop<T, N> = Grammar<T, N, ()>;
+
 #[derive(Debug, Clone)]
-pub struct Grammar<T, N>
+pub struct Grammar<T, N, A>
 where
     T: SymbolType,
     N: Nonterminal,
 {
-    rules: HashMap<N, Vec<RuleBody<T, N>>>,
+    rules: HashMap<N, Vec<Rhs<T, N, A>>>,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct RuleBody<T, N>(pub Vec<Symbol<T, N>>)
-where
-    T: SymbolType,
-    N: Nonterminal;
-
-impl<T, N> RuleBody<T, N>
+pub struct Rhs<T, N, A>
 where
     T: SymbolType,
     N: Nonterminal,
 {
-    pub fn first(&self) -> Option<&Symbol<T, N>> {
-        self.0.first()
-    }
-}
-
-impl<T, N> IntoIterator for RuleBody<T, N>
-where
-    T: SymbolType,
-    N: Nonterminal,
-{
-    type Item = Symbol<T, N>;
-    type IntoIter = vec::IntoIter<Self::Item>;
-    fn into_iter(self) -> Self::IntoIter {
-        self.0.into_iter()
-    }
-}
-
-impl<T, N> FromIterator<Symbol<T, N>> for RuleBody<T, N>
-where
-    T: SymbolType,
-    N: Nonterminal,
-{
-    fn from_iter<I: IntoIterator<Item = Symbol<T, N>>>(iter: I) -> Self {
-        Self(iter.into_iter().collect())
-    }
+    pub body: Vec<Symbol<T, N>>,
+    pub assoc: A,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -76,17 +51,34 @@ where
     }
 }
 
-impl<T, N> Grammar<T, N>
+impl<T, N, A> Grammar<T, N, A>
 where
     T: SymbolType,
     N: Nonterminal,
 {
     // TODO: Return Result with custom error.
-    pub fn new(rules: HashMap<N, Vec<RuleBody<T, N>>>) -> Option<Self> {
-        Some(Self { rules })
+    pub fn new(rules: HashMap<N, Vec<Rhs<T, N, A>>>) -> Option<Self> {
+        // Check that all nonterminals used in rule bodies have their own rules.
+        if rules
+            .iter()
+            .flat_map(|(_, rhs)| rhs)
+            .flat_map(|rhs| &rhs.body)
+            .any(|sy| match sy {
+                Symbol::Nonterminal(n) => rules.get(&n).is_none(),
+                Symbol::Terminal(_) => false,
+            })
+        {
+            None
+        } else {
+            Some(Self { rules })
+        }
     }
 
-    pub fn rules(&self) -> &HashMap<N, Vec<RuleBody<T, N>>> {
+    pub fn rules(&self) -> &HashMap<N, Vec<Rhs<T, N, A>>> {
         &self.rules
     }
+
+    // pub fn predictive_parser(&self) -> Result<LLParser<T, N, A>, ConstructionError> {
+    // LLParser::from_grammar(self)
+    // }
 }
--- lalr/src/lib.rs
+++ lalr/src/lib.rs
@@ -1,5 +1,9 @@
 #![feature(trait_alias)]
 
+mod error;
 mod grammar;
+mod ll;
 
+pub use error::*;
 pub use grammar::*;
+pub use ll::Parser as LLParser;
