36f4ede461d191c58557429f7944b6bf022c27e4
Eric Zhao <21zhaoe@protonmail.com>
2021-01-31 12:45:29 -0500

    asm-ex: Fix tabs

--- asm-ex/hello.s
+++ asm-ex/hello.s
@@ -8,18 +8,18 @@ global _start
 section .text
 
 _start:
-	mov rax, 1				; system call 4: write
-	mov rdi, 1				; file descriptor 1: stdout
-	mov rsi, msg			; string to write
-	mov rdx, msglen		; length of string
-	syscall						; execute system call
+  mov rax, 1        ; system call 4: write
+  mov rdi, 1        ; file descriptor 1: stdout
+  mov rsi, msg      ; string to write
+  mov rdx, msglen   ; length of string
+  syscall           ; execute system call
 
-	mov rax, 60				; system call 60: exit
-	mov rdi, 0				;
-	syscall
+  mov rax, 60       ; system call 60: exit
+  mov rdi, 0        ;
+  syscall
 
 ; data section
 section .rodata
 
-	msg: db "Hello, world!", 10
-	msglen: equ $ - msg
+  msg: db "Hello, world!", 10
+  msglen: equ $ - msg
