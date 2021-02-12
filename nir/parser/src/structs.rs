3a77cb34013ca680dc3e13bf343312d5c892eac3
Eric Zhao <21zhaoe@protonmail.com>
2021-02-11 22:43:06 -0500

    nir: Struct field parsing

--- nir/parser/src/structs.rs
+++ nir/parser/src/structs.rs
@@ -1,8 +1,8 @@
 use crate::error::{ExpectedToken, ParseError};
-use crate::{Parse, ParseInput, ParseResult, Rsv, Separated, Symbol};
+use crate::{Parse, ParseInput, ParseResult, Symbol};
 
-use ast::{FunctionParam, PrimitiveType, Struct, StructField, StructFunction, Type};
-use lexer::{types as ttypes, Reserved, Token};
+use ast::{Struct, StructField};
+use lexer::{types as ttypes, Reserved};
 
 impl<I> Parse<I> for Struct
 where
@@ -50,17 +50,17 @@ where
             };
 
             // Check if member is a function or a field.
-            if input.peek_is(&reserved!(Function)) {
-                // If fn token next, parse as function.
-                let mut function: StructFunction = input.parse()?;
-                function.vis = member_vis;
-                functions.push(function);
-            } else {
-                // Otherwise, parse as struct field.
-                let mut field: StructField = input.parse()?;
-                field.vis = member_vis;
-                fields.push(field);
-            }
+            // if input.peek_is(&reserved!(Function)) {
+            // // If fn token next, parse as function.
+            // let mut function: StructFunction = input.parse()?;
+            // function.vis = member_vis;
+            // functions.push(function);
+            // } else {
+            // Otherwise, parse as struct field.
+            let mut field: StructField = input.parse()?;
+            field.vis = member_vis;
+            fields.push(field);
+            // }
         }
 
         input.consume::<ttypes::RBrace>()?;
@@ -74,59 +74,76 @@ where
     }
 }
 
-impl<I> Parse<I> for StructFunction
+impl<I> Parse<I> for StructField
 where
     I: Iterator<Item = Symbol>,
 {
     #[inline]
     fn parse(input: &mut ParseInput<I>) -> ParseResult<Self> {
-        // Parse visibility.
         let vis = input.parse()?;
-
-        // Consume fn token.
-        input.next_checked(&reserved!(Function), || vec![ereserved!(Function)])?;
-
-        // Parse function name.
         let name = input.parse()?;
 
-        // Conusme left parenthesis.
-        input.consume::<ttypes::LParen>()?;
+        input.consume::<ttypes::Comma>()?;
 
-        // Parse parameters.
-        // Check if self parameter is present.
-        let is_method = if input.peek_is(&Token::Ident("self".to_string())) {
-            // Actually consume the self token.
-            input.next();
-            // Consume the comma.
-            input.consume::<ttypes::Comma>()?;
-            true
-        } else {
-            false
-        };
+        let ty = input.parse()?;
 
-        let params = input
-            .parse::<Separated<FunctionParam, Rsv<ttypes::Comma>>>()?
-            .items;
-
-        // Conusme right parenthesis.
-        input.consume::<ttypes::RParen>()?;
-
-        // Parse the return type (if any).
-        let return_type = match input.consume_opt::<ttypes::Arrow>()? {
-            Some(_) => input.parse()?,
-            None => Type::Primitive(PrimitiveType::Unit),
-        };
-
-        // Parse block.
-        let body = input.parse()?;
-
-        Ok(Self {
-            vis,
-            name,
-            params,
-            return_type,
-            is_method,
-            body,
-        })
+        Ok(Self { vis, name, ty })
     }
 }
+
+// impl<I> Parse<I> for StructFunction
+// where
+// I: Iterator<Item = Symbol>,
+// {
+// #[inline]
+// fn parse(input: &mut ParseInput<I>) -> ParseResult<Self> {
+// // Parse visibility.
+// let vis = input.parse()?;
+
+// // Consume fn token.
+// input.consume::<ttypes::Function>()?;
+
+// // Parse function name.
+// let name = input.parse()?;
+
+// // Conusme left parenthesis.
+// input.consume::<ttypes::LParen>()?;
+
+// // Parse parameters.
+// // Check if self parameter is present.
+// let is_method = if input.peek_is(&Token::Ident("self".to_string())) {
+// // Actually consume the self token.
+// input.next();
+// // Consume the comma.
+// input.consume::<ttypes::Comma>()?;
+// true
+// } else {
+// false
+// };
+
+// let params = input
+// .parse::<Separated<FunctionParam, Rsv<ttypes::Comma>>>()?
+// .items;
+
+// // Conusme right parenthesis.
+// input.consume::<ttypes::RParen>()?;
+
+// // Parse the return type (if any).
+// let return_type = match input.consume_opt::<ttypes::Arrow>()? {
+// Some(_) => input.parse()?,
+// None => Type::Primitive(PrimitiveType::Unit),
+// };
+
+// // Parse block.
+// let body = input.parse()?;
+
+// Ok(Self {
+// vis,
+// name,
+// params,
+// return_type,
+// is_method,
+// body,
+// })
+// }
+// }
