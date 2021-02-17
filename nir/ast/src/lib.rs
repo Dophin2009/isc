6f12641174ca395667df073a7fe70b230477067f
Eric Zhao <21zhaoe@protonmail.com>
2021-02-17 16:35:44 -0500

    nir/ast: Move node definitions to nodes module

--- nir/ast/src/lib.rs
+++ nir/ast/src/lib.rs
@@ -1,22 +1,5 @@
-mod block;
-mod expr;
-mod function;
-mod ident;
-mod program;
-mod structs;
-mod ty;
-mod visibility;
-
-pub mod punctuated;
-
-pub use block::*;
-pub use expr::*;
-pub use function::*;
-pub use ident::*;
-pub use program::*;
-pub use structs::*;
-pub use ty::*;
-pub use visibility::*;
+mod nodes;
+pub use nodes::*;
 
 pub use lexer::types as keywords;
 
---
+++ nir/ast/src/nodes/mod.rs
@@ -0,0 +1,19 @@
+mod block;
+mod expr;
+mod function;
+mod ident;
+mod program;
+mod structs;
+mod ty;
+mod visibility;
+
+pub mod punctuated;
+
+pub use block::*;
+pub use expr::*;
+pub use function::*;
+pub use ident::*;
+pub use program::*;
+pub use structs::*;
+pub use ty::*;
+pub use visibility::*;
