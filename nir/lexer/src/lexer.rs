09e126a057b5e1d76df3513812b6a5c54aab9653
Eric Zhao <21zhaoe@protonmail.com>
2021-02-17 15:53:50 -0500

    nir/lexer: Fix string literal pattern

--- nir/lexer/src/lexer.rs
+++ nir/lexer/src/lexer.rs
@@ -85,7 +85,7 @@ llex::lexer! {
     "false" => literal!(Literal::Boolean(false)),
 
     r"[A-Za-z_][A-Za-z0-9_]*" => Some(Token::Ident(text.to_string())),
-    r#"".*""# => literal!(Literal::Str(text.to_string())),
+    r#""[^"]*""# => literal!(Literal::Str(text.to_string())),
 
     r"[0-9]+" => {
         let n = text.parse().unwrap();
