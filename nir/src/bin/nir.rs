a30a2b38bd5771656b3143bd358ada7211491586
Eric Zhao <21zhaoe@protonmail.com>
2021-02-17 11:49:04 -0500

    nir: Parse from file or stdin

--- nir/src/bin/nir.rs
+++ nir/src/bin/nir.rs
@@ -1,17 +1,27 @@
-use std::io;
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
 
 use nir::Compiler;
+use utf8_chars::BufReadCharsExt;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let compiler = Compiler::new();
 
-    let input = r#"pub struct A {
-        b: u8,
-        c: str,
-    }"#;
+    let stdin = io::stdin();
+    let mut input: Box<dyn BufRead> = match env::args().nth(1) {
+        Some(filepath) => {
+            let file = File::open(filepath)?;
+            Box::new(BufReader::new(file))
+        }
+        None => Box::new(stdin.lock()),
+    };
+
+    // TODO: find better way to do this
+    let chars = input.chars().filter_map(|c| c.ok());
 
     let stderr = io::stderr();
-    compiler.parse_emit(&mut stderr.lock(), input.chars())?;
+    compiler.parse_emit(&mut stderr.lock(), chars)?;
 
     Ok(())
 }
